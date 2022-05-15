4dad030f38ff8fe311ca5d9cb6573fc9c599a910
Melvin John <melvinjohn2001@gmail.com>
2022-05-15 05:01:18 -0700

    fibbo.sh

--- Sem-4/fibo.sh
+++ Sem-4/fibo.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 clear
-    echo "Enter the number of Fibonnaci numbers needed: "
-    read num 
 
-    x=0
-    y=1
-    i=2
+echo "Enter the number of Fibonacci terms to be found: "
+read n
+clear
+x=0
+y=1
+i=2
 
-    while[$i -lt $num]
-    do
-        echo "$x"
-        echo "$y"
-        z = 'expr $x+$y'
-        echo $z
-        x = $y
-        y = $z
-    done
+echo "$x"
+
+while [ $i -lt $n ]
+do
+
+	z=$(($x+$y))
+	echo "$z"
+        x=$y
+	y=$z
+	i=$(($i+1))
+done
+
+
+
