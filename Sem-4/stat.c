d364848592ce90f526d3e3ce1bdb255404a395b6
Melvin John <melvinjohn2001@gmail.com>
2022-06-01 14:03:16 +0530

    Update stat.c

--- Sem-4/stat.c
+++ Sem-4/stat.c
@@ -1,9 +1,40 @@
-#include<stdio.h>
-#include<sys/stat.h>
-int main()
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+
+int main(int argc, char **argv)
 {
-  struct stat sfile; //pointer to stat struct
-  stat("stat.c", &sfile); //stat system call
-  printf("st_mode = %o", sfile.st_mode); //accessing st_mode (data member of stat struct)
-  return 0;
+    if(argc != 2)
+        return 1;
+
+    struct stat fileStat;
+    if(stat(argv[1], &fileStat) < 0)
+        return 1;
+
+    printf("Information for stat3.c\n", argv[1]);
+    printf("---------------------------\n");
+    printf("File Size:\t%dbytes\n", fileStat.st_size);
+    printf("\nBlock size: %d\n", fileStat.st_blksize);
+    printf("\nBlocks: %d\n", fileStat.st_blocks);
+
+    printf("File Permissions: ");
+    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+    printf("\n");
+
+    printf("mtime: %d\n", &fileStat.st_mtime);
+    printf("ctime: %d\n", &fileStat.st_ctime);
+    printf("atime: %d\n", &1fileStat.st_atime);
+
+    return 0;
 }
