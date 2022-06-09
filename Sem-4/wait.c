6c5b0b61c98d4ab40a0e3a8fae8e0f740fa5bdba
Melvin John <melvinjohn2001@gmail.com>
2022-06-09 13:30:34 +0530

    last update

--- Sem-4/wait.c
+++ Sem-4/wait.c
@@ -1,16 +1,24 @@
-#include<stdlib.h>
-#include<sys/wait.h>
 #include<unistd.h>
- 
+#include<sys/types.h>
+#include<stdio.h>
+#include<sys/wait.h>
+
 int main()
 {
-    pid_t cpid;
-    if (fork()== 0)
-        exit(0);           /* terminate child */
-    else
-        cpid = wait(NULL); /* reaping parent */
-    printf("Parent pid = %d\n", getpid());
-    printf("Child pid = %d\n", cpid);
- 
-    return 0;
+pid_t p;
+printf("\nBefore using wait.\n");
+p=fork();
+if(p==0)//child
+{
+printf("I am child having id %d\n",getpid());
+printf("My parent's id is %d\n",getppid());
+}
+else//parent
+{
+wait(NULL);
+printf("After setting wait as NULL\n");
+printf("My child's id is %d\n",p);
+printf("I am parent having id %d\n",getpid());
+}
+printf("\n------------------------------------\n");
 }
