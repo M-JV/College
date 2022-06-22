d66f781d2ffc4476686d55c98995023625d2f132
Melvin John <melvinjohn2001@gmail.com>
2022-06-22 19:56:17 +0530

    with arrival time

--- Sem-4/SFJ.c
+++ Sem-4/SFJ.c
@@ -1,63 +1,46 @@
-#include<stdio.h>
- 
-void main()
+	
+#include <stdio.h>
+  
+int main() 
 {
-    int bt[20],p[20],wt[20],tat[20],i,j,n,total=0,pos,temp;
-    float avg_wt,avg_tat;
-    printf("Enter number of process:");
-    scanf("%d",&n);
- 
-    printf("\nEnter Burst Time:\n");
-    for(i=0;i<n;i++)
-    {
-        printf("p%d:",i+1);
-        scanf("%d",&bt[i]);
-        p[i]=i+1;           //contains process number
-    }
- 
-    //sorting burst time in ascending order using selection sort
-    for(i=0;i<n;i++)
-    {
-        pos=i;
-        for(j=i+1;j<n;j++)
-        {
-            if(bt[j]<bt[pos])
-                pos=j;
-        }
- 
-        temp=bt[i];
-        bt[i]=bt[pos];
-        bt[pos]=temp;
- 
-        temp=p[i];
-        p[i]=p[pos];
-        p[pos]=temp;
-    }
- 
-    wt[0]=0;            //waiting time for first process will be zero
- 
-    //calculate waiting time
-    for(i=1;i<n;i++)
-    {
-        wt[i]=0;
-        for(j=0;j<i;j++)
-            wt[i]+=bt[j];
- 
-        total+=wt[i];
-    }
- 
-    avg_wt=(float)total/n;      //average waiting time
-    total=0;
- 
-    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
-    for(i=0;i<n;i++)
-    {
-        tat[i]=bt[i]+wt[i];     //calculate turnaround time
-        total+=tat[i];
-        printf("\np%d\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
-    }
- 
-    avg_tat=(float)total/n;     //average turnaround time
-    printf("\n\nAverage Waiting Time=%f",avg_wt);
-    printf("\nAverage Turnaround Time=%f\n",avg_tat);
+      int arrival_time[10], burst_time[10], temp[10];
+      int i, smallest, count = 0, time, limit;
+      double wait_time = 0, turnaround_time = 0, end;
+      float average_waiting_time, average_turnaround_time;
+      printf("nEnter the Total Number of Processes:t");
+      scanf("%d", &limit); 
+      printf("nEnter Details of %d Processesn", limit);
+      for(i = 0; i < limit; i++)
+      {
+            printf("nEnter Arrival Time:t");
+            scanf("%d", &arrival_time[i]);
+            printf("Enter Burst Time:t");
+            scanf("%d", &burst_time[i]); 
+            temp[i] = burst_time[i];
+      }
+      burst_time[9] = 9999;  
+      for(time = 0; count != limit; time++)
+      {
+            smallest = 9;
+            for(i = 0; i < limit; i++)
+            {
+                  if(arrival_time[i] <= time && burst_time[i] < burst_time[smallest] && burst_time[i] > 0)
+                  {
+                        smallest = i;
+                  }
+            }
+            burst_time[smallest]--;
+            if(burst_time[smallest] == 0)
+            {
+                  count++;
+                  end = time + 1;
+                  wait_time = wait_time + end - arrival_time[smallest] - temp[smallest];
+                  turnaround_time = turnaround_time + end - arrival_time[smallest];
+            }
+      }
+      average_waiting_time = wait_time / limit; 
+      average_turnaround_time = turnaround_time / limit;
+      printf("nnAverage Waiting Time:t%lfn", average_waiting_time);
+      printf("Average Turnaround Time:t%lfn", average_turnaround_time);
+      return 0;
 }
