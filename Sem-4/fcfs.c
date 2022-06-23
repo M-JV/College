86d6a3b1d09a6e6cf6e9838fe6c9ca11d88a2fe3
Melvin John <melvinjohn2001@gmail.com>
2022-06-23 09:04:33 +0000

    Update fcfs.c

--- Sem-4/fcfs.c
+++ Sem-4/fcfs.c
@@ -1,59 +1,51 @@
 #include<stdio.h>
 int main()
 {
-    int  p[10],at[10],bt[10],ct[10],tat[10],wt[10],i,j,temp=0,n;
-    float awt=0,atat=0;
-    printf("enter no of proccess you want:");
+    int AT[10],BT[10],WT[10],TT[10],n,i;
+    int burst=0,cmpl_T;
+    float Avg_WT,Avg_TT,Total=0;
+    printf("Enter number of the process\n");
     scanf("%d",&n);
-    printf("enter %d process:",n);
-    for(i=0;i<n;i++){
-    scanf("%d",&p[i]);}
-
-    printf("enter %d arrival time:",n);
-    for(i=0;i<n;i++){
-    scanf("%d",&at[i]);}
-
-    printf("enter %d burst time:",n);
-    for(i=0;i<n;i++){
-    scanf("%d",&bt[i]);}
-    // sorting at,bt, and process according to at
-    for(i=0;i<n;i++){
-     for(j=0;j<(n-i);j++){
-      if(at[j]>at[j+1]){
-        temp=p[j+1];
-        p[j+1]=p[j];
-        p[j]=temp;
-        temp=at[j+1];
-        at[j+1]=at[j];
-        at[j]=temp;
-        temp=bt[j+1];
-        bt[j+1]=bt[j];
-        bt[j]=temp;}
-     }
+    printf("Enter Arrival time and Burst time of the process\n");
+    printf("AT BT\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d",&AT[i],&BT[i]);
     }
-    /* calculating 1st ct */
-    ct[0]=at[0]+bt[0];
-    /* calculating 2 to n ct */
-    for(i=1;i<n;i++){  
-      //when proess is ideal in between i and i+1
-    temp=0;
-        if(ct[i-1]<at[i]){
-        temp=at[i]-ct[i-1];}
-     ct[i]=ct[i-1]+bt[i]+temp;
-    }
-    /* calculating tat and wt */
-    printf("\np\t A.T\t B.T\t C.T\t TAT\t WT");
-    for(i=0;i<n;i++){
-    tat[i]=ct[i]-at[i];
-    wt[i]=tat[i]-bt[i];
-    atat+=tat[i];
-    awt+=wt[i];}
 
-    atat=atat/n;
-    awt=awt/n;
-    for(i=0;i<n;i++){
-      printf("\nP%d\t %d\t %d\t %d \t %d \t %d",p[i],at[i],bt[i],ct[i],tat[i],wt[i]);}
-    printf("\naverage turnaround time is %f",atat);
-    printf("\naverage wating timme is %f",awt);
+    // Logic for calculating Waiting time
+    for(i=0;i<n;i++)
+    {
+        if(i==0)
+            WT[i]=AT[i];
+        else
+            WT[i]=burst-AT[i];
+        burst+=BT[i];
+        Total+=WT[i];
+    }
+    Avg_WT=Total/n;
+
+    // Logic for calculating Turn around time
+    //initially completion time = 0
+    //for each process comp time = burst time - arrival time
+    cmpl_T=0;
+    Total=0;
+    for(i=0;i<n;i++)
+    {
+        cmpl_T+=BT[i];
+        TT[i]=cmpl_T-AT[i];
+        Total+=TT[i];
+    }
+    Avg_TT=Total/n;
+
+    // printing of outputs
+
+    printf("Process\t Waiting_time\t TurnA_time\n");
+    for(i=0;i<n;i++)
+    {
+        printf("\t%d\t\t%d\t\t%d\n",i+1,WT[i],TT[i]);
+    }
+    printf("Average waiting time is : %f\n",Avg_WT);
+    printf("Average turn around time is : %f\n",Avg_TT);
     return 0;
 }
